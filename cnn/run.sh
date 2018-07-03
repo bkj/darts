ed22fae560462ae6df6321a604a7e0ae84918d7f
Ben Johnson <bkj.322@gmail.com>
2018-07-03 00:11:18 -0400

    running bkj experiment again

--- cnn/run.sh
+++ cnn/run.sh
@@ -3,7 +3,7 @@
 # run.sh
 
 cd cnn
-CUDA_VISIBLE_DEVICES=7 python train_search.py --unrolled
+CUDA_VISIBLE_DEVICES=6 python train_search.py --unrolled
 
 # CUDA_VISIBLE_DEVICES=7 python train.py --arch BKJ_DARTS
 # CUDA_VISIBLE_DEVICES=6 python train.py --arch DARTS
